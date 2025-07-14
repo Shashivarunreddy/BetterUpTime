2066875e45ba7c391f8288fa677f7f79b903e9c8
Shashivarunreddy <shashivarun6154@gmail.com>
2025-07-14 05:10:29 +0000

    first commit

---
+++ apps/api/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+const app = express();
+
+app.post("/website" , (req,res) => {
+
+});
+
+app.get("/status/websiteId" , (req, res) =>{
+
+});
+
+app.listen(process.env.PORT || 3000)
